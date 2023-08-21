caa3cd206535ac770966fc45cef5f5d3392fdca5
mr-hritu <83459565+mr-hritu@users.noreply.github.com>
2023-08-21 19:26:25 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -1,7 +1,25 @@
 from pyrogram import Client, filters
 from pyrogram.types import CallbackQuery
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup 
+from pyrogram.errors import UserBannedInChannel, UserNotParticipant
 import constants
 
+
+@Client.on_message(filters.text & filters.private & filters.incoming)
+async def fore(c, m):
+      try:
+        chat = await c.get_chat_member(-1001785446911, m.from_user.id)
+        if chat.status=="kicked":
+           await c.send_message(chat_id=m.chat.id, text="You are Banned ☹️\n\n📝 If u think this is an ERROR message in @Privates_chats", reply_to_message_id=m.id)
+           m.stop_propagation()
+      except UserBannedInChannel:
+         return await c.send_message(chat_id=m.chat.id, text="Hai you made a mistake so you are banned from channel so you are banned from me too 😜")
+      except UserNotParticipant:
+          button = [[InlineKeyboardButton('Updates Channel 🇮🇳', url='https://t.me/Private_Bots')]]
+          markup = InlineKeyboardMarkup(button)
+          return await c.send_message(chat_id=m.chat.id, text="""Hai bro,\n\nYou must join my channel for using me.\n\nPress this button to join now 👇""", reply_markup=markup)
+      m.continue_propagation()
 
 @Client.on_callback_query(filters.regex(r"^back$"))
 async def backtostart(bot: Client, query: CallbackQuery):
