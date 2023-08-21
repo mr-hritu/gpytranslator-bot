c7f08183a0f4575d14983ad224bd0719e0c1a08b
mr-hritu <83459565+mr-hritu@users.noreply.github.com>
2023-08-21 19:23:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
-API_ID: int = your api id
-API_HASH: str = "your api hash"
-TOKEN: str = "bot token"
+API_ID: int = 28888037
+API_HASH: str = "9fbe164b5591df05fbd8577e3b1d6d21"
+TOKEN: str = "6660071929:AAH6JvMfr3uNEEOVkR1YTZq7c5tPrx-Jc64"
 
-sudousers: list = [id, id]
+sudousers: list = [6387027582,5190902724]
 sudofilter = filters.user(sudousers)
 db_url: str = "sqlite://userlanguages.db"
